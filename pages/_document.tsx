f824ff4cc0759102eb16472fc92b948ad31f71e4
Neil Gebhard <neilgebhard@Neils-Air.lan>
2023-04-27 12:27:30 +1000

    Fixed a bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
+      <Head />
       <body className='bg-neutral-50'>
         <Main />
         <NextScript />
