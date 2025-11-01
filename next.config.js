20a545667087adb3cb6cfe849b81ff966a8f3f0a
Neil Gebhard <neilgebhard@gmail.com>
2025-11-01 15:26:50 +0700

    Update supabase domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['frwqvmguatavxorvfhlx.supabase.co'],
+    domains: ['zusvyfkzywauthictelq.supabase.co'],
   },
   typescript: {
     ignoreBuildErrors: true,
