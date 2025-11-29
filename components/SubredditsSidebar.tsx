cf9851377de3b61a36a6bd9df456e82933179439
Neil Gebhard <neilgebhard@gmail.com>
2025-11-29 10:49:12 -0700

    Add types to subreddit sidebar component

--- components/SubredditsSidebar.tsx
+++ components/SubredditsSidebar.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
-
-interface Subreddit {
-  name: string
-}
+import { Subreddit } from '@/types/models'
+import { ROUTES } from '@/constants/routes'
 
 interface SubredditsSidebarProps {
   subreddits: Subreddit[]
@@ -14,9 +12,13 @@ export default function SubredditsSidebar({ subreddits }: SubredditsSidebarProps
       <div className='bg-white rounded border border-neutral-300 p-5 hidden sm:block'>
         <h2 className='text-xl font-bold mb-3'>Subreddits</h2>
         <ul>
-          {subreddits.map(({ name }, i) => (
-            <Link className='block hover:underline' href={`/r/${name}`} key={i}>
-              /r/{name}
+          {subreddits.map((subreddit) => (
+            <Link
+              className='block hover:underline'
+              href={ROUTES.SUBREDDIT(subreddit.name)}
+              key={subreddit.id}
+            >
+              /r/{subreddit.name}
             </Link>
           ))}
         </ul>
